0a516f438a32e43b94aa21f089adc4a9e7f9580f
epolovec <ema.polovec@gmail.com>
2021-11-23 04:19:01 +0200

    first commit

---
+++ ND1.js
@@ -0,0 +1,27 @@
+function Car (make, model, color, fuelType) {
+    this.make = make;
+    this.model = model;
+    this.color = color;
+    this.fuelType = fuelType;
+}
+
+Car.prototype.markModel = function () {
+    return {
+        make: this.make,
+        model: this.model
+    }
+}
+
+Car.prototype.kuroTikrinimas = function () {
+    if (this.fuelType === 'Diesel') {
+        return "Automobilis varomas dyzeliu";
+    } else if (this.fuelType === 'Petrol') {
+        return "Automobilis varomas benzinu";
+    } else if (this.fuelType === 'Electricity') {
+        return "Automobilis varomas elektra"
+    }
+}
+
+const car = new Car ('BMW', 'X5', 'Black', 'Diesel');
+console.log(car.markModel());
+console.log(car.kuroTikrinimas());
---
+++ ND2.js
@@ -0,0 +1,26 @@
+class Car {
+    constructor(make, model, color, fuelType) {
+        this.make = make;
+        this.model = model;
+        this.color = color;
+        this.fuelType = fuelType;
+    }
+    markModel() {
+        return {
+            make: this.make,
+            model: this.model
+        }
+    }
+    kuroTikrinimas() {
+        if (this.fuelType === 'Diesel') {
+            return "Automobilis varomas dyzeliu";
+        } else if (this.fuelType === 'Petrol') {
+            return "Automobilis varomas benzinu";
+        } else if (this.fuelType === 'Electricity') {
+            return "Automobilis varomas elektra"
+        }
+    }
+}
+const car = new Car ('BMW', 'X5', 'Black', 'Diesel');
+console.log(car.markModel());
+console.log(car.kuroTikrinimas());
